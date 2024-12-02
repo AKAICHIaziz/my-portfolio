33842bab9330dcb30b2b181c70268f3a7cb8c735
Med Aziz Akaichi <akaichiaziz10@gmail.com>
2024-12-02 18:28:56 +0100

    test

--- src/components/ProfileDetails.jsx
+++ src/components/ProfileDetails.jsx
@@ -9,11 +9,11 @@ export default function ProfileDetails() {
 
             <div>
                 <h1 className="font-bold text-base sm:text-xl text-foreground">Who am I</h1>
-                <p className="text-foreground/70 text-justify text-sm sm:text-base">
-                    I'm Akaichi Mohamed Aziz, a passionate software developer and a final-year computer science student, set to graduate this summer with a bachelor&apos;s degree.
+                {/* <p className="text-foreground/70 text-justify text-sm sm:text-base">
+                    I m Akaichi Mohamed Aziz, a passionate software developer and a final-year computer science student, set to graduate this summer with a bachelor&apos;s degree.
                     My journey started with a love for technology and a curiosity about how things work. Today, I&apos;m dedicated to creating solutions that make life easier and more connected.
                     My approach combines creativity, precision, and a constant drive for growth, enabling me to contribute meaningfully to projects that inspire and push boundaries. I value collaboration, adaptability, and the opportunity to make a positive impact through my work.
-                </p>
+                </p> */}
             </div>
 
         </div>
