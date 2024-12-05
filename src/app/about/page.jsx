4c81556f3db43e78a59d2b2c9e399b4fd5746b4d
Med Aziz Akaichi <akaichiaziz10@gmail.com>
2024-12-05 12:21:05 +0100

    about page 1

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,17 +1,12 @@
 
 import ProfileDetails from "@/components/ProfileDetails";
-import GitHubCalendar from 'react-github-calendar';
 
 export default function About() {
     return (
-        <div className="w-full h-screen flex justify-center pt-24 bg-background ">
+        <div className="w-full h-screen flex justify-center pt-14 sm:pt-24 bg-background ">
 
-            <div className="w-9/12 flex flex-col items-center">
-
+            <div className="w-full sm:w-9/12 h-fit">
                 <ProfileDetails />
-
-                
-
             </div>
 
         </div>
--- src/components/ProfileDetails.jsx
+++ src/components/ProfileDetails.jsx
@@ -1,19 +1,19 @@
 import Image from "next/image";
-import my_image from "../../public/img.png"
+import my_image from "../../public/img2.jpg"
 
 export default function ProfileDetails() {
     return (
-        <div className="w-full flex flex-row gap-10 items-center pb-10">
+        <div className="w-full flex flex-col items-center gap-5 sm:gap-0 p-5 sm:p-2">
 
-            <Image src={my_image} alt="Profile image" className="w-44 hidden rounded-full lg:block" />
+            <Image src={my_image} alt="Profile image" className="w-24 sm:w-36 rounded-full" />
 
-            <div>
-                <h1 className="font-bold text-base sm:text-xl text-foreground">Who am I</h1>
-                {/* <p className="text-foreground/70 text-justify text-sm sm:text-base">
-                    I m Akaichi Mohamed Aziz, a passionate software developer and a final-year computer science student, set to graduate this summer with a bachelor&apos;s degree.
+            <div className="flex flex-col gap-2 sm:gap-0">
+                <h1 className="font-semibold text-sm sm:text-lg text-foreground">Who am I ?</h1>
+                <p className="text-foreground/70 text-justify text-xs sm:text-base">
+                    I&apos;m Akaichi Mohamed Aziz, a passionate software developer and a final-year computer science student, set to graduate this summer with a bachelor&apos;s degree.
                     My journey started with a love for technology and a curiosity about how things work. Today, I&apos;m dedicated to creating solutions that make life easier and more connected.
                     My approach combines creativity, precision, and a constant drive for growth, enabling me to contribute meaningfully to projects that inspire and push boundaries. I value collaboration, adaptability, and the opportunity to make a positive impact through my work.
-                </p> */}
+                </p>
             </div>
 
         </div>
