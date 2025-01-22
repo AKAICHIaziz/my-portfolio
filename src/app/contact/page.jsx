0b96da3761e88af2eb7aa83352c0d3f704b577c6
Med Aziz Akaichi <135503030+AKAICHIaziz@users.noreply.github.com>
2025-01-22 13:08:18 +0100

    Update page.jsx

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,7 +1,9 @@
 export default function Contact() {
     return (
         <div className="pt-40">
-            Contact page
+            <p>The contact page is not avaible yet</p>
+            <p>Scroll to the bottom of the Home page, there is a form that you can fill to reach me.</p>
+            <p><b>Comming Soon ...</b></p>
         </div>
     )
 } 
